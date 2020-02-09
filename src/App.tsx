2750257d2046238cd6d8e9614f4fb428601f1239
ph5500 <philfives@gmail.com>
2020-02-09 14:50:03 -0700

    added a game board with squares

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,79 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Board Game</h1>
+        <p>class Square extends React.Component {
+          render() {
+            return (
+              <button className="square">
+                {/* TODO */}
+              </button>
+            );
+          }
+        }
+
+        class Board extends React.Component {
+          renderSquare(i:any) {
+            return <Square />;
+          }
+
+          render() {
+            const status = "Board game";
+
+            return (
+              <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                  {this.renderSquare(0)}
+                  {this.renderSquare(1)}
+                  {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                  {this.renderSquare(3)}
+                  {this.renderSquare(4)}
+                  {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                  {this.renderSquare(6)}
+                  {this.renderSquare(7)}
+                  {this.renderSquare(8)}
+                  {this.renderSquare(9)}
+                </div>
+                <div className="board-row">
+                  {this.renderSquare(10)}
+                  {this.renderSquare(11)}
+                  {this.renderSquare(12)}
+              </div>
+            );
+          }
+        }
+
+        class Game extends React.Component {
+          render() {
+            return (
+              <div className="game">
+                <div className="game-board">
+                  <Board />
+                </div>
+                <div className="game-info">
+                  <div>{/* status */}</div>
+                  <ol>{/* TODO */}</ol>
+                </div>
+              </div>
+            );
+          }
+        }</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
            <code>Goodbye World</code>
         </p>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -27,9 +92,12 @@ const App = () => {
 
         Google
         </a>
+
+
       </header>
     </div>
   );
 }
+
 
 export default App;
